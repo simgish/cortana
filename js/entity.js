0c5981fae8a4925b1350258fa85a41356e405469
simgish <ryandoughty@gmail.com>
2014-07-24 14:25:30 -0400

    entity init with x, y

--- js/entity.js
+++ js/entity.js
@@ -10,11 +10,22 @@ this.Cortana = this.Cortana || {};
 		this.id = null;
 		this.type = _args.type || null;
 		this.name = _args.name || null;
+		this.x = _args.x || 0;
+		this.y = _args.y || 0;
 		this.state = null;
 		this.dirty = false;
 	};
 
 	Entity.prototype = {
+
+		init: function(x, y) {
+			console.log('init at ' + x + y);
+		},
+
+		draw: function(x, y) {
+
+		},
+
 		say: function(text) {
 			console.log(text);
 		},
