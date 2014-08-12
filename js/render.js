94d93713f14c083f21ca99ae880b1e40dc27a547
Ryan Doughty <ryandoughty@gmail.com>
2014-08-11 21:04:21 -0400

    render update()

--- js/render.js
+++ js/render.js
@@ -2,11 +2,14 @@
 	'use strict';
 
 	var Render = function(game) {
-		
+		this.game = game;
 	};
 
 	Render.prototype = {
-		
+
+		update: function(dt) {
+
+		}	
 	}
 
 	Render.prototype.constructor = Render;
