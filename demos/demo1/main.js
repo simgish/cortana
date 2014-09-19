accab4b3856a68f0541d25be5d10e7dbc5245ff5
simgish <ryandoughty@gmail.com>
2014-09-19 00:06:55 -0400

    infinite scrolling background demo

--- demos/demo1/main.js
+++ demos/demo1/main.js
@@ -4,7 +4,22 @@ var bg = new Cortana.Entity({
 	name: 'bg',
 	pos: {x: 0, y: 0},
 	sprite: null,
-	vel: 1.0,
+	vel: 10,
+
+	init: function() {
+		this.sprite = new Cortana.Sprite('../images/citybg2.png', 500, 281, this.pos.x, this.pos.y);
+	},
+
+	update: function(dt) {
+		this.sprite.draw(this.pos.x, this.pos.y);
+	}
+});
+
+var bg2 = new Cortana.Entity({
+	name: 'bg',
+	pos: {x: Cortana.width, y: 0},
+	sprite: null,
+	vel: 10,
 
 	init: function() {
 		this.sprite = new Cortana.Sprite('../images/citybg2.png', 500, 281, this.pos.x, this.pos.y);
@@ -63,6 +78,7 @@ var player = new Cortana.Entity({
 });
 
 game.add(bg);
+game.add(bg2);
 game.add(coin);
 game.add(player);
 
@@ -73,6 +89,16 @@ function update(dt) {
 
 	if (t > bg.vel) {
 		bg.pos.x -= bg.vel;
+		bg2.pos.x -= bg2.vel;
+
+		if (bg.pos.x <= -Cortana.width) {
+			bg.pos.x = Cortana.width;
+		}
+
+		if (bg2.pos.x <= -Cortana.width) {
+			bg2.pos.x = Cortana.width;
+		}
+
 		lastTime = dt;
 	}
 }
