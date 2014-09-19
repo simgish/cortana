c6e2850e7ca0ad44ee70cf3426f1013aae742490
simgish <ryandoughty@gmail.com>
2014-09-19 00:35:47 -0400

    parallax scrolling demo

--- demos/demo1/main.js
+++ demos/demo1/main.js
@@ -4,10 +4,10 @@ var bg = new Cortana.Entity({
 	name: 'bg',
 	pos: {x: 0, y: 0},
 	sprite: null,
-	vel: 10,
+	vel: 2,
 
 	init: function() {
-		this.sprite = new Cortana.Sprite('../images/citybg2.png', 500, 281, this.pos.x, this.pos.y);
+		this.sprite = new Cortana.Sprite('../images/citybg2.png', 500, 313, this.pos.x, this.pos.y);
 	},
 
 	update: function(dt) {
@@ -16,13 +16,43 @@ var bg = new Cortana.Entity({
 });
 
 var bg2 = new Cortana.Entity({
-	name: 'bg',
+	name: 'bg2',
 	pos: {x: Cortana.width, y: 0},
 	sprite: null,
-	vel: 10,
+	vel: 2,
 
 	init: function() {
-		this.sprite = new Cortana.Sprite('../images/citybg2.png', 500, 281, this.pos.x, this.pos.y);
+		this.sprite = new Cortana.Sprite('../images/citybg2.png', 500, 313, this.pos.x, this.pos.y);
+	},
+
+	update: function(dt) {
+		this.sprite.draw(this.pos.x, this.pos.y);
+	}
+});
+
+var starfield = new Cortana.Entity({
+	name: 'starfield',
+	pos: {x: 0, y: -250},
+	sprite: null,
+	vel: 0.5,
+
+	init: function() {
+		this.sprite = new Cortana.Sprite('../images/starfield.png', 500, 313, this.pos.x, this.pos.y);
+	},
+
+	update: function(dt) {
+		this.sprite.draw(this.pos.x, this.pos.y);
+	}
+});
+
+var starfield2 = new Cortana.Entity({
+	name: 'starfield2',
+	pos: {x: Cortana.width, y: -250},
+	sprite: null,
+	vel: 0.5,
+
+	init: function() {
+		this.sprite = new Cortana.Sprite('../images/starfield.png', 500, 313, this.pos.x, this.pos.y);
 	},
 
 	update: function(dt) {
@@ -52,7 +82,7 @@ var player = new Cortana.Entity({
 	canCollide: true,
 	
 	init: function() {
-		this.sprite = new Cortana.Sprite('../images/ghost.png', 32, 32, this.pos.x, this.pos.y);
+		this.sprite = new Cortana.Sprite('../images/ship.png', 32, 32, this.pos.x, this.pos.y);
 	},
 	
 	update: function(dt) {
@@ -79,6 +109,8 @@ var player = new Cortana.Entity({
 
 game.add(bg);
 game.add(bg2);
+game.add(starfield);
+game.add(starfield2);
 game.add(coin);
 game.add(player);
 
@@ -90,6 +122,8 @@ function update(dt) {
 	if (t > bg.vel) {
 		bg.pos.x -= bg.vel;
 		bg2.pos.x -= bg2.vel;
+		starfield.pos.x -= starfield.vel;
+		starfield2.pos.x -= starfield2.vel;
 
 		if (bg.pos.x <= -Cortana.width) {
 			bg.pos.x = Cortana.width;
@@ -97,6 +131,14 @@ function update(dt) {
 
 		if (bg2.pos.x <= -Cortana.width) {
 			bg2.pos.x = Cortana.width;
+		}
+
+		if (starfield.pos.x <= -Cortana.width) {
+			starfield.pos.x = Cortana.width;
+		}
+
+		if (starfield2.pos.x <= -Cortana.width) {
+			starfield2.pos.x = Cortana.width;
 		}
 
 		lastTime = dt;
