97486eb6436b9d716f61b1471336c58c6170c5b7
simgish <ryandoughty@gmail.com>
2014-09-21 14:50:09 -0400

    separate player into own file

--- demos/demo1/main.js
+++ demos/demo1/main.js
@@ -75,44 +75,11 @@ var coin = new Cortana.Entity({
 	}
 });
 
-var player = new Cortana.Entity({
-	name: 'player',
-	pos: {x: 140, y: 65},
-	sprite: null,
-	canCollide: true,
-	
-	init: function() {
-		this.sprite = new Cortana.Sprite('../images/ship.png', 32, 32, this.pos.x, this.pos.y);
-	},
-	
-	update: function(dt) {
-
-		if (game.input.pressed.up && this.pos.y > 0) {
-			this.pos.y -= 5;
-		}
-
-		if (game.input.pressed.down && (this.pos.y + this.sprite.height) < Cortana.height) {
-			this.pos.y += 5;
-		}
-
-		if (game.input.pressed.left && this.pos.x > 0) {
-			this.pos.x -= 5;
-		}
-
-		if (game.input.pressed.right && (this.pos.x + this.sprite.width) < Cortana.width) {
-			this.pos.x += 5;
-		}
-
-		this.sprite.draw(this.pos.x, this.pos.y);
-	}
-});
-
 game.add(bg);
 game.add(bg2);
 game.add(starfield);
 game.add(starfield2);
 game.add(coin);
-game.add(player);
 
 var lastTime = 0;
 
