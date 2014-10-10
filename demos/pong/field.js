21186a924f06845f1eaa1cd89883a4df6866add8
simgish <ryandoughty@gmail.com>
2014-10-10 18:06:48 -0400

    add images and field.js

---
+++ demos/pong/field.js
@@ -0,0 +1,16 @@
+var field = new Cortana.Entity({
+	name: 'starfield2',
+	pos: {x: 0, y: 0},
+	sprite: null,
+	vel: 0,
+
+	init: function() {
+		this.sprite = new Cortana.Sprite('images/field.png', 800, 600, this.pos.x, this.pos.y);
+	},
+
+	update: function(dt) {
+		this.sprite.draw(this.pos.x, this.pos.y);
+	}
+});
+
+game.add(field);
